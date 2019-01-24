6c9a1783e3b77c72da5dae58aad68b8672f636c1
Kacper Zieliński <kacper.zielinski@wat.edu.pl>
2019-01-24 23:31:18 +0100

    OnFormSubmit -> prawidłowe wykrozystanie metody

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,14 @@ import React, { Component } from "react";
 export default class SearchBar extends Component {
   state = { term: "" };
 
+  OnFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+  };
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.OnFormSubmit} className="ui form">
           <div className="field">
             <label>Wyszukaj obrazek</label>
             <input
